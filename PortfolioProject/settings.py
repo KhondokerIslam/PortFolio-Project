0c60ee742c5c52ddf3c8d35406a53bd00e959d08
Khondoker Islam <shanislam6@gmail.com>
2019-10-04 08:49:42 +0600

    Added local support

--- PortfolioProject/settings.py
+++ PortfolioProject/settings.py
@@ -134,3 +134,8 @@ STATIC_URL = '/experience/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #Saving images in this folder
 MEDIA_URL =  '/media/' #if someone wants to choose from the previous image, where they can do it from
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
